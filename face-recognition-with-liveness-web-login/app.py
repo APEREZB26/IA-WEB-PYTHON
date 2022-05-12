f020d47b038c8af690f9554624241a219a5dfc76
Yactayo <syactayocrisostomo@gmail.com>
2022-05-12 16:53:32 -0500

    [ADD] controllers register, login and main

--- face-recognition-with-liveness-web-login/app.py
+++ face-recognition-with-liveness-web-login/app.py
@@ -1,57 +1,96 @@
-#jorge
+# jorge
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+import time
 
 # import our model from folder
 from face_recognition_and_liveness.face_liveness_detection.face_recognition_liveness_app import recognition_liveness
 
 app = Flask(__name__)
-app.secret_key = 'web_app_for_face_recognition_and_liveness' # something super secret
+app.secret_key = 'web_app_for_face_recognition_and_liveness'  # something super secret
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Users(db.Model):
-    username = db.Column(db.String(100), primary_key=True)
-    name = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    dni = db.Column(db.String(100))
     password = db.Column(db.String(100))
+    card = db.Column(db.String(100))
+
 
 @app.route('/')
 def index():
     return redirect(url_for('login'))
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        session.pop('name', None)
-        username = request.form['username']
+        card = request.form['card']
+        dni = request.form['dni']
         password = request.form['password']
-        user = Users.query.filter_by(username=username).first()
-        print(user)
-        if user is not None and user.password == password:
-            session['name'] = user.name # store variable in session
+        user = Users.query.filter_by(
+            card=card, dni=dni).first()
+
+        # print(user.id)
+        if user and check_password_hash(user.password, password):
             detected_name, label_name = recognition_liveness('face_recognition_and_liveness/face_liveness_detection/liveness.model',
-                                                    'face_recognition_and_liveness/face_liveness_detection/label_encoder.pickle',
-                                                    'face_recognition_and_liveness/face_liveness_detection/face_detector',
-                                                    'face_recognition_and_liveness/face_recognition/encoded_faces.pickle',
-                                                    confidence=0.5)
-            if user.name == detected_name and label_name == 'real':
+                                                             'face_recognition_and_liveness/face_liveness_detection/label_encoder.pickle',
+                                                             'face_recognition_and_liveness/face_liveness_detection/face_detector',
+                                                             'face_recognition_and_liveness/face_recognition/encoded_faces.pickle',
+                                                             confidence=0.5)
+            if user.fullname == detected_name and label_name == 'real':
+                session['id'] = user.id
+                session['fullname'] = user.fullname
                 return redirect(url_for('main'))
             else:
-                return render_template('login_page.html', invalid_user=True, username=username)
+                return render_template('login_page.html', invalid_user=True, username=user.fullname)
         else:
             return render_template('login_page.html', incorrect=True)
-
     return render_template('login_page.html')
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        # img = request.files['image']
+        card = request.form['card']
+        fullname = request.form['fullname']
+        email = request.form["email"]
+        dni = request.form['dni']
+        password = request.form['password']
+        token = int(round(time.time() * 1000))
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = Users(fullname=fullname, email=email,
+                         dni=dni, card=card, password=hashed_password)
+        print(new_user)
+        db.session.add(new_user)
+        db.session.commit()
+        print("registradoooo")
+
+        os.mkdir(f'face_recognition_and_liveness/face_recognition/dataset/{fullname}')
+
+        return redirect(url_for('login'))
+
     return render_template('register.html')
+
 
 @app.route('/main', methods=['GET'])
 def main():
-    name = session['name']
-    return render_template('main_page.html', name=name)
+    try:
+        id = session['id']
+        fullname = session['fullname']
+        print(f'Este es el iddddd {id}')
+        return render_template('main_page.html', id=id, fullname=fullname)
+    except Exception as e:
+        return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     db.create_all()
